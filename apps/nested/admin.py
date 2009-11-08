2c7f618c40545a6b4c779cf2cf6c214aa84c637d
Ronny Abraham <this.ronny@gmail.com>
2009-11-08 19:00:05 +0200

    new file:   apps/nested/admin.py

---
+++ apps/nested/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+
+from nested.models import * 
+
+
+from languages.models import LanguageChoice
+
+num_langs = LanguageChoice.objects.filter(is_activated=True).count()
+
+class LowerInline(admin.StackedInline):
+    model = Lower
+    max_num=1
+    
+class LowerAdmin(admin.ModelAdmin):
+    class DescInline(admin.TabularInline):
+        model = Lower.Desc
+        max_num = num_langs
+
+    class TitleInline(admin.TabularInline):
+        model = Lower.Title
+        max_num = num_langs
+
+    #list_display = ['name', 'title', 'description']
+    inlines = [DescInline, TitleInline]
+
+class LowerAdminMod(admin.ModelAdmin):
+    form = LowerForm
+    class DescInline(admin.TabularInline):
+        model = Lower.Desc
+        max_num = num_langs
+
+    class TitleInline(admin.TabularInline):
+        model = Lower.Title
+        max_num = num_langs
+
+    #list_display = ['name', 'title', 'description']
+    inlines = [DescInline, TitleInline]
+
+
+class TopAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    inlines = [LowerInline]
+
+admin.site.register(Lower, LowerAdmin)
+admin.site.register(Top, TopAdmin)
+
