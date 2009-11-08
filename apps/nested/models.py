3645411b6f576588d28974bc133229dd6b41b8b9
Ronny Abraham <this.ronny@gmail.com>
2009-11-08 18:58:49 +0200

    modified:   apps/nested/models.py

--- apps/nested/models.py
+++ apps/nested/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 from languages.models import ACharField, ATextField
 
+
+# form imports
+from django.forms import ModelForm
+from django import forms
+
 # Create your models here.
 class Top(models.Model):
 
@@ -34,3 +39,23 @@ class Lower(models.Model):
     name = models.CharField(max_length="200")
     title = models.CharField(max_length="200")
     description = models.TextField()
+
+
+class LowerForm(forms.ModelForm):
+    name = forms.CharField(max_length=100)
+    lanchoice = forms.CharField(widget=forms.Textarea)
+    class Meta:
+        model = Lower
+        fields = ['name']
+    def clean(self):
+        self.cleaned_data['description'] = self.cleaned_data['lanchoice']
+        del self.cleaned_data['lanchoice']
+        return self.cleaned_data
+
+    def save(self, commit=True):
+        lower = ModelForm.save(self, False)
+        lower.description = self.cleaned_data['description']
+        lower.save()
+        return ModelForm.save(self,commit)
+        
+        
