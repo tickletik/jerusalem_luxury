8756d57b4fa6e296db89d06fb9f6eb300b8f1faa
Ronny Abraham <this.ronny@gmail.com>
2009-12-05 22:14:54 +0200

    modified:   apps/building/models.py

--- apps/building/models.py
+++ apps/building/models.py
@@ -8,17 +8,21 @@ class Block(models.Model):
     def __unicode__(self):
         return "%s" % self.name
 
-    class Desc_Block(ATextField):
-        primary = models.ForeignKey("Block")
-        class Meta:
-            verbose_name="Description"
-            verbose_name_plural="Description"
-    Desc=Desc_Block
-
 class Building(models.Model):
     block = models.ForeignKey(Block)
     name = models.CharField(max_length=200)
+    image_large = models.ImageField(upload_to="img/nested/large")
+    image_thumb = models.ImageField(upload_to="img/nested/thumb")
     
-
     def __unicode__(self):
         return "Building %s, Block %s" % (name, block.name)
+
+class Tenant(models.Model):
+    building = models.ForeignKey(Building)
+
+    language_choice = models.ForeignKey(LanguageChoice)
+    title = models.CharField(max_length=200)
+    desc = models.TextField()
+
+    def __unicode(self):
+        return "title = %s, desc = %s, language = %s" % (self.title, self.desc, self.language_choice.name)
