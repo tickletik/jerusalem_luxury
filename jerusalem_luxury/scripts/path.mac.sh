b2a170bdfc75acc0dd1bd71825eb13b66af7b554
Ronny Abraham <this.ronny@gmail.com>
2010-04-25 16:54:56 +0300

    renamed:    data/data.xml -> ../data/data.xml 	renamed:    data/postoutput.txt -> ../data/postoutput.txt 	renamed:    data/scratch.py -> ../data/scratch.py 	deleted:    scripts/path.mac.sh 	deleted:    scripts/path.webfaction.sh 	modified:   settings/mac.py 	renamed:    scripts/_sql/nested.sql -> ../scripts/_sql/nested.sql 	renamed:    scripts/nested.sh -> ../scripts/nested.sh 	renamed:    scripts/recreate_database.sql -> ../scripts/recreate_database.sql 	renamed:    scripts/reset.sh -> ../scripts/reset.sh 	renamed:    scripts/syncdb.sh -> ../scripts/syncdb.sh

--- jerusalem_luxury/scripts/path.mac.sh
+++
@@ -1,8 +0,0 @@
-PATH=$PATH:"/opt/projects/django/jerusalem_luxury.git"
-export PATH
-
-DJANGO_MODULE_SETTINGS="jerusalem_luxury.settings"
-export DJANGO_MODULE_SETTINGS
-
-PYTHONPATH="/opt/projects/django/jerusalem_luxury.git/apps"
-export PYTHONPATH
--- jerusalem_luxury/scripts/path.webfaction.sh
+++
@@ -1,8 +0,0 @@
-PATH=$PATH:"/home/jldevadmin/webapps/django"
-export PATH
-
-DJANGO_MODULE_SETTINGS="jerusalem_luxury.settings"
-export DJANGO_MODULE_SETTINGS
-
-PYTHONPATH="/home/jldevadmin/apps"
-export PYTHONPATH
