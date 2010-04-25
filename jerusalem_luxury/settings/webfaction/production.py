bb28ed57dfca34a8d12ac00605754d81f18c3830
Ronny <this.ronny@gmail.com>
2010-04-25 11:11:02 -0500

    copied:     settings/webfaction.py -> settings/webfaction/development.py 	renamed:    settings/webfaction.py -> settings/webfaction/production.py 	renamed:    ../scripts/syncdb.sh -> ../scripts/syncdb/mac.sh 	new file:   ../scripts/syncdb/webfaction/development.sh 	new file:   ../scripts/syncdb/webfaction/production.sh

--- jerusalem_luxury/settings/webfaction/production.py
+++ jerusalem_luxury/settings/webfaction/production.py
@@ -13,9 +13,9 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'jldevadmin_db'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'jldevadmin_db'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'panterra'         # Not used with sqlite3.
+DATABASE_NAME = 'samia_prod'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'samia_prod'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'bd59dd52'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
@@ -38,12 +38,12 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/jldevadmin/media'
+MEDIA_ROOT = '/home/samia/webapps/media_production'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://www.jerusalem-luxurydev.com/media'
+MEDIA_URL = '/media'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
@@ -69,7 +69,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'jerusalem_luxury.urls'
 
 TEMPLATE_DIRS = (
-    "/home/jldevadmin/templates",
+    "/home/samia/webapps/django_production/templates",
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
---
+++ scripts/syncdb/webfaction/production.sh
@@ -0,0 +1,1 @@
+python manage.py syncdb --noinput; python manage.py loaddata $DJANGO/fixtures/sites.json loaddata $DJANGO/fixtures/realty.json
